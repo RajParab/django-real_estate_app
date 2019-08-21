49bc485a60563480cbc22ae2bffd228960e3940c
Raj Parab <raj.parab99@yahoo.com>
2019-08-21 22:54:50 +0530

    Added hosts

--- src/housing/settings.py
+++ src/housing/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#cx_%%j2+k2brqx(52mnc5u$@!@5btmyko^*8j31$a=pf+_31w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
